bc4a80983674bcfb2e5518d46eb1c82392ae5b28
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-26 20:49:42 -0300

    feat(layouts): change gradient color

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-// import DefaultLayout from '~/pages/_layouts/default';
+import DefaultLayout from '~/pages/_layouts/default';
 
 import Home from '~/pages/Home';
 
 export default function Routes() {
+  const Layout = DefaultLayout;
+
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Route
+        path="/"
+        exact
+        render={(props) => (
+          <Layout>
+            <Home />
+          </Layout>
+        )}
+      />
     </Switch>
   );
 }
